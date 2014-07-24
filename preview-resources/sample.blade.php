6cbe1303891c5704ac0583b671b561f0ff065906
Hirohisa Kawase <hirokws@gmail.com>
2014-07-24 10:32:00 +0900

    プレビューの404とインデックスページを追加

---
+++ preview-resources/sample.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('title')
+レイアウトサンプル
+@stop
+
+@section('content')
+  <div class="row">
+    <h1>Bootsrapレイアウトサンプル</h1>
+  </div>
+  <div class="row">
+    <p>到着いたしました!</p>
+  </div>
+@stop
