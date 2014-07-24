5996509a4a2bf8162d174f2f354aaa1b75b4bc90
Hirohisa Kawase <hirokws@gmail.com>
2014-07-24 12:04:19 +0900

    watchdocシェル中のserviceの出力をnullに流し、安定して動作させた。

--- restart-watchdoc.sh
+++ restart-watchdoc.sh
@@ -4,9 +4,9 @@ while :
 do
     if [ -e /home/home/restart-required ]
     then
-        service nginx force-reload
-        service php5-fpm stop
-        service php5-fpm start
+        service nginx force-reload &> /dev/null
+        service php5-fpm stop &> /dev/null
+        service php5-fpm start &> /dev/null
         rm /home/home/restart-required
     fi
     sleep 30
